0f7cec58f7f22b4d841ea10e0a822e7374f21ac7
bcaul <bcaulfield68@gmail.com>
2025-07-10 13:47:02 +0100

    Attempt 3

--- app/main.py
+++ app/main.py
@@ -1,28 +1,27 @@
 import sys
 
+BUILTINS = {
+    "exit": lambda code=0, *_: sys.exit(int(code)),
+    "echo": lambda *args: print(" ".join(args)),
+    "type": lambda cmd, *_: print(f"{cmd} is a shell builtin")
+    if cmd in BUILTINS
+    else print(f"{cmd}: not found"),
+}
+
 
 def main():
-
     while True:
-
-        # Uncomment this block to pass the first stage
         sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-        # Wait for user input
-        command = input()
-        if command == "exit 0":
-            sys.exit(0)
-
-        elif command.startswith("echo "):
-            print(command[5:])
-        
-        elif command == "echo":
-            print()
+        usr_input = input().split()
+        cmd = usr_input[0]
+        args = usr_input[1:]
+        if cmd in BUILTINS:
+            BUILTINS[cmd](*args)
         else:
-            print(f"{command}: command not found")
-        
-        
-    
+            print(f"{cmd}: command not found")
+
 
 if __name__ == "__main__":
     main()
